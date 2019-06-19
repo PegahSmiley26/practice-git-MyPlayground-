0d3134002060ebb158401720a49f675dcd189a31
Apple <apple@Apples-MacBook-Pro-9.local>
2019-06-19 15:46:50 -0500

    Iteration Two Completed

--- AviatrixApp/Aviatrix.swift
+++ AviatrixApp/Aviatrix.swift
@@ -14,6 +14,12 @@ class Aviatrix {
     var data = AviatrixData()
     var location = "St. Louis"
     var target = ""
+    var distanceTraveled = 0.0
+    var fuelLevel = 5000.0
+    var maxFuel = 5000.0
+    var milesPerGallon = 0.4
+    var amountFilled = Double()
+    var fuelCost = 0.0
     init(authorName : String) {
         author = authorName
     }
@@ -21,12 +27,21 @@ class Aviatrix {
         return true
     }
     
-    func refuel() {
-        
+    func refuel() -> Double {
+        amountFilled = plane.maxFuel - plane.fuelLevel
+        fuelLevel = maxFuel
+        fuelCost = fuelCost + amountFilled * data.fuelPrices[location]!;
+        amountFilled = Double(round(10.0*amountFilled/10))
+        fuelCost = Double(round(100*fuelCost/100))
+        return data.fuelPrices[location]!;
     }
     
     func flyTo(destination : String) {
-     location = destination
+        distanceTraveled = distanceTraveled + Double(data.knownDistances[location]![destination]!);
+    fuelLevel = maxFuel - distanceTraveled * milesPerGallon;
+    location = destination
+        
+    
     }
     
     func distanceTo(target : String) -> Int {
