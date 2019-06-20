40aafe022154c663641906b357c5091831b83dea
Apple <apple@Apples-MacBook-Pro-9.local>
2019-06-20 15:05:30 -0500

    emojinalproject0

---
+++ EmojionalProject/EmojionalProject/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  EmojionalProject
+//
+//  Created by Apple on 6/20/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func uSA(_ sender: UIButton) {
+
+    }
+    @IBAction func gERMANY(_ sender: UIButton) {
+    }
+    @IBAction func rUSSIA(_ sender: UIButton) {
+    }
+    @IBAction func cANDA(_ sender: UIButton) {
+    }
+    @IBAction func greatBRITAIN(_ sender: UIButton) {
+    }
+    
+
+}
+
