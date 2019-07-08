a48dfe1fc2929af050a43181569d78076f207be6
Anagha Narayanan <anagha.naraya@gmail.com>
2019-07-08 13:53:31 -0500

    First commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+//this helps to keep comments and track your code
+
+//intention: learning
+
+//var str = "Hello, playground"
+//
+//var name = "Pegah"
+//
+//print("Hi \(name), I'm so happy to be at KWK.")
+
+//var name1 = "pegah"
+//var name2 = "cady"
+//
+//print("Hi we are \(name1) and \(name2), we are happy to be here.")
+
+
+////Intention: working with numbers
+////Data type: ints and doubles
+//
+//var int = 2
+//var double = 3.0
+//
+//print(Double(int) * double)
+//
+////cant just do int * double bc they are two different number types
+
+
+//var a = 1
+//var b = 2
+//var c = 3
+//
+//print((a+b+c)/3)
+
+
+
+
+
+
+
+
+
